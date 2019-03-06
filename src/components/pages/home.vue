23a58a3a49fc8b48e3ee9ef8b5326d5ca7522ceb
wlh <wlh@qq.com>
2019-03-06 21:14:37 +0800

    初始化仓库

---
+++ src/components/pages/home.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+      sldkdnfklnsdlk  
+    </div>
+</template>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+// 引入element-ui组件
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css'
+import router from './router'
+// 关闭生产阶段的生产输出
+Vue.config.productionTip = false
+// 使用element组件
+Vue.use(ElementUI);
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
